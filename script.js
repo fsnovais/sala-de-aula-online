a6d5327bf8d631931c4c17f572b5995511bf78be
Felipe Novais <felipekun213@gmail.com>
2021-03-01 01:28:26 -0300

    implementação do chat

---
+++ script.js
@@ -0,0 +1,34 @@
+const socket = io('http://localhost:3000')
+const messageContainer = document.getElementById('message-container')
+const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message-input')
+
+const name = prompt('Insira seu nome completo?')
+appendMessage('Você se conectou')
+socket.emit('new-user', name)
+
+socket.on('chat-message', data => {
+  appendMessage(`${data.name}: ${data.message}`)
+})
+
+socket.on('user-connected', name => {
+  appendMessage(`${name} se conectou`)
+})
+
+socket.on('user-disconnected', name => {
+  appendMessage(`${name} se desconectou`)
+})
+
+messageForm.addEventListener('submit', e => {
+  e.preventDefault()
+  const message = messageInput.value
+  appendMessage(`Você: ${message}`)
+  socket.emit('send-chat-message', message)
+  messageInput.value = ''
+})
+
+function appendMessage(message) {
+  const messageElement = document.createElement('div')
+  messageElement.innerText = message
+  messageContainer.append(messageElement)
+}
