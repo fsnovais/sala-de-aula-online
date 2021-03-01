ec0f3e216c24f25b0a4386786c856dd4b8c3732c
Felipe Novais <felipekun213@gmail.com>
2021-03-01 09:04:34 -0300

    porta alterada para 3334

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:3334')
+const socket = io('http://localhost:3334',{secure: true})
 const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
