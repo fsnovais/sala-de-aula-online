1aa7dcfb38c7005a2a34b326b63acdd903747cc7
Felipe Novais <felipekun213@gmail.com>
2021-03-01 09:02:49 -0300

    porta alterada para 3334

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("express")();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const io = require('socket.io',{secure: true})(http);
 const cors = require('cors');
 
 app.get('/', (req, res) => {
